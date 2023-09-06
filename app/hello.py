59947aec77f097eaa6e9c2742350fb7386ecadac
Jisu Park <zsu58@icloud.com>
2023-09-06 23:59:35 +0900

    fix: bug

--- app/hello.py
+++ app/hello.py
@@ -4,4 +4,7 @@ def hello(name="World"):
   return f"Hello {name}!"
 
 if __name__ == "__main__":
-  print(hello(sys.argv[1]))
+  if len(sys.argv) > 1:
+    print(hello(sys.argv[1]))
+  else:
+    print(hello())
